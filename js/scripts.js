57e52ef6e17573ac1ae48a3f79ba2afc8991dc87
Agnieszka Nowacka <aguo84@gmail.com>
2018-01-12 23:31:36 +0100

    Add html and js files

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+function drawXmasTree(height) {
+	for (var i = 1; i < (height + 1); i++) {
+		var star = "";
+		for (var j = 0; j < (height - i); j++) {
+			star += " ";
+		}
+		for (var k = 0; k < i; k++) {
+			if (i % 2 === 1 && k % 2 === 0) {
+				star += "o ";
+			} else {
+				star += "* ";
+			}
+		}
+	console.log(star);
+	}
+}
+
+drawXmasTree(15);
